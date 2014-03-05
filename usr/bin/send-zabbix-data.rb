71b56adcf20a448f30a1231466c21ed912585281
Andy Grimm <agrimm@redhat.com>
2014-03-05 10:11:57 -0500

    use new oo-cgroup-read report command for much better performance

--- usr/bin/send-zabbix-data.rb
+++ usr/bin/send-zabbix-data.rb
@@ -9,45 +9,6 @@ ZABBIX_PORT    = ENV['ZABBIX_SERVER_PORT']
 ZABBIX_SENDER  = ENV['OPENSHIFT_ZABBIX_AGENT_DIR'] + '/bin/zabbix_sender'
 ZABBIX_RUN_DIR = ENV['OPENSHIFT_ZABBIX_AGENT_DIR'] + '/run'
 
-def get_cgroup_data
-  keys = [ 'cpuacct.stat',
-           'cpuacct.usage',
-           'cpu.cfs_period_us',
-           'cpu.cfs_quota_us',
-           'cpu.rt_period_us',
-           'cpu.rt_runtime_us',
-           'cpu.stat',
-           'memory.failcnt',
-           'memory.limit_in_bytes',
-           'memory.max_usage_in_bytes',
-           'memory.memsw.failcnt',
-           'memory.memsw.limit_in_bytes',
-           'memory.memsw.max_usage_in_bytes',
-           'memory.memsw.usage_in_bytes',
-           'memory.stat',
-           'memory.swappiness',
-           'memory.usage_in_bytes',
-         ]
-
-  # This is ugly, because the file formats are not consistent
-  data = {}
-
-  keys.each do |k|
-    if k.end_with?('.stat')
-      data[k] = Hash.new()
-      %x[ oo-cgroup-read #{k} ].lines.each do |line|
-        subkey, subval = line.split()
-        data[k][subkey] = subval.strip()
-      end
-    else
-      data[k] = %x[ oo-cgroup-read #{k} ].strip()
-    end
-    # puts k
-  end
-
-  return data
-end
-
 def get_quota_data
   res = nil
 
@@ -106,8 +67,7 @@ def send_data(entries, verbose = true)
 end
 
 
-json_data = get_cgroup_data
-# puts JSON.dump(get_cgroup_data)
+json_data = JSON.load(%x[ oo-cgroup-read report ])
 entries = Hash.new()
 json_data.each do |k,v|
   next if k.start_with?('memory.stat.total_')
